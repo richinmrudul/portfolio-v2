6b34cfdf0c9d21bc0a5d94ad45e5de9065462b15
Richin Mrudul <richinm@gmail.com>
2025-11-19 11:08:01 -0500

    integrate to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import Projects from "./components/Projects";
 
 const App: React.FC = () => {
@@ -8,6 +9,7 @@ const App: React.FC = () => {
     <div className="font-sans bg-slate-900 text-white">
       <Navbar />
       <Hero />
+      <About />
       <Projects />
     </div>
   );
